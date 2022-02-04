8301fd0760d8c7b48f643cf5186268d26410ab38
Pekka Jääskeläinen <pekka.jaaskelainen@parmance.com>
2022-02-04 12:14:38 +0200

    Device-side HIP abort()
    Works on Intel LZ and OpenCL. Fails on PoCL likely due to the known
    issue with program scope variables accessed from multiple different
    kernels.

--- llvm_passes/HipPasses.cpp
+++ llvm_passes/HipPasses.cpp
@@ -1,5 +1,6 @@
 // Define a pass plugin that runs a collection of HIP passes.
 
+#include "HipAbort.h"
 #include "HipDefrost.h"
 #include "HipDynMem.h"
 #include "HipTexture.h"
@@ -20,6 +21,7 @@ static void addFullLinkTimePasses(ModulePassManager &MPM) {
   MPM.addPass(HipTextureExternReplaceNewPass());
   MPM.addPass(HipPrintfToOpenCLPrintfPass());
   MPM.addPass(createModuleToFunctionPassAdaptor(HipDefrostPass()));
+  MPM.addPass(HipAbortPass());
   // This pass must appear after HipDynMemExternReplaceNewPass.
   MPM.addPass(HipGlobalVariablesPass());
 }
--- samples/printf/strings.cc
+++ samples/printf/strings.cc
@@ -1,6 +1,6 @@
 /*
-Copyright (c) 2020 - 2021 Advanced Micro Devices, Inc. All rights reserved
-and 2022 Pekka Jääskeläinen / Parmance for Argonne National Laboratory
+Copyright (c) 2022 Pekka Jääskeläinen / Parmance for Argonne National
+Laboratory
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
