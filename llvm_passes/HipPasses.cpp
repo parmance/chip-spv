43790c6cd021e65f3ebc290cc8b2d4d6499fab7a
Henry Linjamäki <henry.linjamaki@parmance.com>
2022-02-24 14:30:46 +0200

    Redesign texture function pass

--- llvm_passes/HipPasses.cpp
+++ llvm_passes/HipPasses.cpp
@@ -2,25 +2,57 @@
 
 #include "HipDefrost.h"
 #include "HipDynMem.h"
-#include "HipTexture.h"
 #include "HipStripCompilerUsed.h"
 #include "HipPrintf.h"
 #include "HipGlobalVariables.h"
+#include "HipTextureLowering.h"
 
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
+#include "llvm/Transforms/IPO/Inliner.h"
+#include "llvm/Transforms/Scalar/DCE.h"
+#include "llvm/Transforms/IPO/GlobalDCE.h"
+#include "llvm/Transforms/Scalar/SROA.h"
 
 using namespace llvm;
 
+// A pass that removes noinline and optnone attributes from functions.
+class RemoveNoInlineOptNoneAttrsPass
+    : public PassInfoMixin<RemoveNoInlineOptNoneAttrsPass> {
+public:
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM) {
+    for (auto &F : M) {
+      F.removeFnAttr(Attribute::NoInline);
+      F.removeFnAttr(Attribute::OptimizeNone);
+    }
+    return PreservedAnalyses::none();
+  }
+  static bool isRequired() { return true; }
+};
+
 static void addFullLinkTimePasses(ModulePassManager &MPM) {
   // Run a collection of passes run at device link time.
-  MPM.addPass(HipStripCompilerUsedPass());
   MPM.addPass(HipDynMemExternReplaceNewPass());
-  MPM.addPass(HipTextureExternReplaceNewPass());
+
+  // Prepare device code for texture function lowering which does not yet work
+  // on non-inlined code and local variables of hipTextureObject_t type.
+  MPM.addPass(RemoveNoInlineOptNoneAttrsPass());
+  // Increase getInlineParams argument for more aggressive inlining.
+  MPM.addPass(ModuleInlinerWrapperPass(getInlineParams(1000)));
+  MPM.addPass(createModuleToFunctionPassAdaptor(SROA()));
+
+  MPM.addPass(HipTextureLoweringPass());
+
   MPM.addPass(createModuleToFunctionPassAdaptor(HipPrintfToOpenCLPrintfPass()));
   MPM.addPass(createModuleToFunctionPassAdaptor(HipDefrostPass()));
   // This pass must appear after HipDynMemExternReplaceNewPass.
   MPM.addPass(HipGlobalVariablesPass());
+
+  // Remove dead code left over by HIP lowering passes and kept alive by
+  // llvm.compiler.used intrinsic variable.
+  MPM.addPass(HipStripCompilerUsedPass());
+  MPM.addPass(createModuleToFunctionPassAdaptor(DCEPass()));
+  MPM.addPass(GlobalDCEPass());
 }
 
 extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
