548d09d7127dd651cf5a88f7cf6a0686d14dc30b
Henry Linjamäki <henry.linjamaki@parmance.com>
2022-04-11 11:49:37 +0300

    HIP-passes: strip llvm.used global intrinsics

--- llvm_passes/HipStripUsedIntrinsics.cpp
+++ llvm_passes/HipStripUsedIntrinsics.cpp
@@ -1,15 +1,15 @@
-// LLVM pass to remove llvm.compiler.used intrinsic variables (see LLVM lang ref
-// for details) from HIP device code modules.
+// LLVM pass to remove llvm.used and llvm.compiler.used intrinsic variables (see
+// LLVM lang ref for details) from HIP device code modules.
 //
 // LLVM/Clang generates these in some cases.  We remove them because they also
 // generate illegal address space casts (when the IR is passed to llvm-spirv
-// translator tool).  It is safe to remove llvm.compiler.used variables as they
-// are meaningless outside the LLVM.
+// translator tool).  It is safe to remove llvm(.compiler).used variables as
+// they are meaningless for SPIR-V.
 //
 // This pass is likely not needed when SPIR-V backend land on LLVM in the
 // future.
 
-#include "HipStripCompilerUsed.h"
+#include "HipStripUsedIntrinsics.h"
 
 #if LLVM_VERSION_MAJOR >= 14
 #include "llvm/Pass.h"
@@ -19,11 +19,11 @@ using namespace llvm;
 
 namespace {
 
-bool stripCompilerUsedVar(Module &M) {
-  auto *GV = M.getGlobalVariable("llvm.compiler.used");
+bool stripGlobalVar(Module &M, StringRef GVName) {
+  auto *GV = M.getGlobalVariable(GVName);
   if (!GV)
     return false;
-  assert(GV->getNumUses() == 0 && "Unexpected uses of llvm.compiler.used");
+  assert(GV->getNumUses() == 0 && "GV has uses.");
   GV->eraseFromParent();
   return true;
 }
@@ -33,7 +33,10 @@ public:
   static char ID;
   HipStripCompilerUsedLegacyPass() : ModulePass(ID) {}
 
-  bool runOnModule(Module &M) override { return stripCompilerUsedVar(M); }
+  bool runOnModule(Module &M) override {
+    return stripGlobalVar(M, "llvm.used") |
+           stripGlobalVar(M, "llvm.compiler.used");
+  }
 
   StringRef getPassName() const override {
     return "Strip llvm.compiler.used variables.";
@@ -51,9 +54,9 @@ static RegisterPass<HipStripCompilerUsedLegacyPass>
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 
-PreservedAnalyses HipStripCompilerUsedPass::run(Module &M,
-                                                ModuleAnalysisManager &AM) {
-  if (stripCompilerUsedVar(M))
+PreservedAnalyses HipStripUsedIntrinsicsPass::run(Module &M,
+                                                  ModuleAnalysisManager &AM) {
+  if (stripGlobalVar(M, "llvm.used") | stripGlobalVar(M, "llvm.compiler.used"))
     return PreservedAnalyses::none();
   return PreservedAnalyses::all();
 }
@@ -66,7 +69,7 @@ llvmGetPassPluginInfo() {
                 [](StringRef Name, ModulePassManager &FPM,
                    ArrayRef<PassBuilder::PipelineElement>) {
                   if (Name == "hip-strip-compiler-used") {
-                    FPM.addPass(HipStripCompilerUsedPass());
+                    FPM.addPass(HipStripUsedIntrinsicsPass());
                     return true;
                   }
                   return false;
